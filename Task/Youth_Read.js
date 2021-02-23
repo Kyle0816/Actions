0c8c5ffe19403e0865810cb0c5401c90a029329a
ziyong <61528323+Kyle0816@users.noreply.github.com>
2021-02-23 10:18:47 +0800

    Update Youth_Read.js

--- Task/Youth_Read.js
+++ Task/Youth_Read.js
@@ -58,7 +58,6 @@ YouthBody = bodys.split('&');
     bjTime = new Date(timestamp).toLocaleString('zh',{hour12:false,timeZoneName: 'long'});
     console.log(`\n === 脚本执行 ${bjTime} ===\n`);
   $.log("\n  您共获取"+ReadArr.length+"次阅读请求，任务开始\n")
-}
       
  !(async () => {
   if (!ReadArr[0]) {
